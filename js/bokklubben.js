99ee418827dfe8bf86703068248fd1bcc6994c87
Tua-Stenberg-NTI-Johanneberg <94751365+tuastenberg@users.noreply.github.com>
2022-02-22 10:20:01 +0100

    22/2

---
+++ js/bokklubben.js
@@ -0,0 +1,8 @@
+function showmenu(){
+    let menu = document.querySelector("nav.menu");
+    menu.classList.toggle("show");
+
+    let layer = document.querySelector(".fade-layer");
+    layer.classList.toggle("visible")
+
+}
