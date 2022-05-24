12c3eed6ca5c7b36eb9b639f2c2164fe760dbb4a
Tua-Stenberg-NTI-Johanneberg <94751365+tuastenberg@users.noreply.github.com>
2022-05-24 13:19:25 +0200

    24/5

--- js/bokklubben.js
+++ js/bokklubben.js
@@ -1,8 +1,13 @@
+let layer = document.querySelector(".fade-layer")
+layer.addEventListener("click", showmenu)
+
+let button = document.querySelector(".menu-button")
+button.addEventListener("click", showmenu)
+
 function showmenu(){
     let menu = document.querySelector("nav.menu");
     menu.classList.toggle("show");
 
     let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible")
-
 }
